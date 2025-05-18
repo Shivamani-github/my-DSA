5299524afcad1e690e855e996e72934eca767194
Shivamani Madhana <165813761+Shivamani-github@users.noreply.github.com>
2025-05-18 19:32:04 +0530

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,11 @@
+public class ifelse{
+    public static void main(String args[]){
+        String vscode="coding";
+        if(vscode.equals("coding")){
+            System.out.println("correct");
+        }
+        else{
+            System.out.println("incorrect");
+        }
+    }
+}
---
+++ variable.java
@@ -0,0 +1,9 @@
+public class variable
+{
+    public static void main(String args[]){
+        String coder="shivamani";
+        int hi=1;
+        System.out.println(coder);
+        System.out.println(hi);
+    }
+}
